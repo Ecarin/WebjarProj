338943d11fb720bb7ad578bbc1bbc6b1d7fc7c6e
Ecarin <3carin@gmail.com>
2023-07-01 00:27:03 +0330

    swagger little change

--- WebjarProj/Program.cs
+++ WebjarProj/Program.cs
@@ -2,13 +2,27 @@ using WebjarProj.Services.Implementations;
 using WebjarProj.Services.Interfaces;
 using WebjarProj.Data;
 using WebjarProj.Mapping;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "Webjar API",
+            Version = "v1",
+            Description = "An API to perform Ecommerce operations",
+            Contact = new OpenApiContact
+            {
+                Name = "Amin Ansari",
+                Email = "3carin@gmail.com",
+            },
+        });
+    });
 
 // Register DbContext
 builder.Services.AddDbContext<WebjarDbContext>();
